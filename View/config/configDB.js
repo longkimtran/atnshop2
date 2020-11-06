27a2b7fece2069b9c91ae4af7a51831d958b664e
longkimtran <73920119+longkimtran@users.noreply.github.com>
2020-11-06 15:12:36 +0700

    Update configDB.js

--- View/config/configDB.js
+++ View/config/configDB.js
@@ -1,16 +1,16 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb://localhost:27017/newshop',
-        dbname : "newshop",
+        urldb : 'mongodb://localhost:27017/Lion-Shop',
+        dbname : "Lion-Shop",
         dbusername : "",
         dbpassword : "",
     },
 
     clouddb: {
-        urldb : 'mongodb://localhost:27017/newshop',
-        dbname : "newshop",
-        dbusername : "",
-        dbpassword : "",
+        urldb : 'mongodb+srv://longkimtran:btQd33lgrP92gc4h@cluster0.zysed.mongodb.net/Lion-Shop?retryWrites=true&w=majority',
+        dbname : "Lion-Shop",
+        dbusername : "longkimtran",
+        dbpassword : "btQd33lgrP92gc4h",
     }
 
 };
