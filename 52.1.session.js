868be407d7bb283c02e262bb0874c1d11e827bdc
Kim Long Tran Huu <longthkgcs18852@fpt.edu.vn>
2020-11-08 11:43:14 +0700

    #

--- 52.1.session.js
+++ 52.1.session.js
@@ -129,24 +129,6 @@ app.get('/order', orderPage);
 function orderPage(req, res) {
     var xcontent = "";
 
-    console.log('\t ... get ORDER INF ! ');
-
-    var strtext = req.cookies.cart_itemlist;
-    xcontent += "<BR><p> " + strtext + "</p>";
-    //
-    strtext = atob(strtext);
-    xcontent += "<BR>atob <p> " + strtext + "</p>";
-    //
-    strtext = escape(strtext);
-    xcontent += "<BR>escape <p> " + strtext + "</p>";
-    //
-    strtext = decodeURIComponent(strtext);
-    xcontent += "<BR>decodeURIComponent <p> " + strtext + "</p>";
-    ///
-    var itemlist  = JSON.parse(strtext);
-
-    console.log("\n\t ", xcontent);
-
     var total = 0;
 
     if(req.session.cart != null) {
